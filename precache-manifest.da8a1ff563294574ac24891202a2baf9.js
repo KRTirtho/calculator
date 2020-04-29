f8143087af983bc5d31c33d9ee3ed7cfe7fc36d8
KR Tirtho <kbroy2016@gmail.com>
2020-04-29 23:40:40 +0600

    Updates

---
+++ precache-manifest.da8a1ff563294574ac24891202a2baf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8ecd470b4509b092cccc43375f74dd",
+    "url": "/project_calculator_fcc/index.html"
+  },
+  {
+    "revision": "fb25bf2cc6ad110ce682",
+    "url": "/project_calculator_fcc/static/css/main.12f4ff4a.chunk.css"
+  },
+  {
+    "revision": "48703ab1b5845a666017",
+    "url": "/project_calculator_fcc/static/js/2.014a0f90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project_calculator_fcc/static/js/2.014a0f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb25bf2cc6ad110ce682",
+    "url": "/project_calculator_fcc/static/js/main.5fe27f41.chunk.js"
+  },
+  {
+    "revision": "d6c22a40a9c5e20991b6",
+    "url": "/project_calculator_fcc/static/js/runtime-main.9f922c55.js"
+  }
+]);
