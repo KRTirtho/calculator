ebf5829ba38ca9a421e3dd0b3331aabe9dbe9a1c
KR Tirtho <kbroy2016@gmail.com>
2020-04-29 23:34:17 +0600

    Alpha Stage

--- src/App.js
+++ src/App.js
@@ -1,79 +1,184 @@
 import React from 'react';
 import './App.css';
 
-const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-const urlImg = 'https://thesimpsonsquoteapi.glitch.me/quotes'
-
-// const fetchQuote = fetch()
-
 class App extends React.Component{
   constructor(props){
-    super(props)
+    super(props);
     this.state = {
-      data: [],
-      loaded: false,
-      updateData: null,
-      imgData: [],
-      imgPos: [],
-      loaded: false
+      currentNumber: '0',
+      decimalFlag: false,
+      operatorFlag: false,
+      limitFlag: false
     }
-    this.handleClick = this.handleClick.bind(this)
-  }  
-
-  componentDidMount(){
-     fetch(url)
-     .then(res=> res.json())
-     .then(json =>{
-       this.setState({
-         loaded : true,
-         data: json.quotes
-       })
-     }).catch(err=> console.error(err +' from '+url+err));
-     fetch(urlImg)
-     .then(res=> res.json())
-     .then(json =>{
-       this.setState({
-         loadedImg : true,
-         imgData: json[0].image,
-         imgPos: json[0].characterDirection
-       })
-     }).catch(err=> console.error('Failed to load image from'+urlImg+err))
-  }
-  handleClick(){
-    const randomNumb = Math.floor(Math.random() * this.state.data.length)
-    const newQuote = this.state.data[randomNumb]
-    fetch(urlImg)
-     .then(res=> res.json())
-     .then(json =>{
-       this.setState({
-         loadedImg : true,
-         imgData: json[0].image,
-         updateData: newQuote,
-         imgPos: json[0].characterDirection
-       })
-     }).catch(err=> console.error('Failed to load image from'+urlImg+err))
+    this.handleClick = this.handleClick.bind(this);
+    // this.clearDisplay = this.clearDisplay.bind(this);
+    // this.totalAnswer = this.totalAnswer.bind(this);
+    // this.backSpace = this.backSpace.bind(this);
   }
 
+handleClick(e){
+  let buttonName = e.target.value
+  let {currentNumber, operatorFlag, decimalFlag} = this.state
+  switch(true){
+      case buttonName === "0" ||
+           buttonName === "1" ||
+           buttonName === "2" ||
+           buttonName === "3" ||
+           buttonName === "4" ||
+           buttonName === "5" ||
+           buttonName === "6" ||
+           buttonName === "7" ||
+           buttonName === "8" ||
+           buttonName === "9" :
+      if(this.state.currentNumber!=="0"){
+      currentNumber += buttonName
+      operatorFlag = false
+      }else{
+      currentNumber = buttonName
+      }
+         break
+    case buttonName === "+" ||
+         buttonName === "-" || 
+         buttonName === "*" ||
+         buttonName === "/" :
+         if(!this.state.operatorFlag){
+           currentNumber += buttonName
+           operatorFlag = true
+           decimalFlag = false
+         }else{
+           let replacer = currentNumber.slice(0, currentNumber.length-1);
+           currentNumber = replacer;
+           currentNumber += buttonName
+         }
+         break
+    case buttonName === ".":
+         if(!this.state.decimalFlag){    //Triggering the Value Of Decimal if Decimal is false then append it to 
+          currentNumber += buttonName;    //the Display or current Number & soon after that make it true so  it will 
+          decimalFlag = true; //not get added after clicking it for the first time😎😎 
+         }
+         break
+    case buttonName === "clear":
+         currentNumber = '0'
+         operatorFlag = false;
+          decimalFlag = false
+         break     
+    case buttonName === '=':
+         currentNumber = eval(this.state.currentNumber)
+         operatorFlag = false;
+         decimalFlag = true;
+         break
+  }
+  this.setState({decimalFlag})
+  this.setState({operatorFlag})
+  this.setState({currentNumber})
+}
+
+// totalAnswer(e){
+//   if(this.state.input !== ''){
+//     this.setState({
+//       output: eval(this.state.input)
+//     })
+//     // this.setState({
+//     //   input: 
+//     // })
+//   }
+// }
+
+
+// backSpace(e){
+//   if(this.state.input !== ''){
+//     this.setState({
+//       input: this.state.input.substring(0, this.state.input.length-1)
+//     })
+//   }
+// }
+
+// clearDisplay(e){
+//     this.setState({
+//       input: '',
+//       output: '0'
+//     })
+// }
   render(){
-    var {loaded, data, loadedImg} = this.state
-    if(!loaded && !loadedImg){
-      return (
-        <div id="loading">Loading</div>
-      )
-    }
-    else{
-      return (
-        <div className="bg" >
-          <div id="quote-box">
-              {this.state.imgPos === 'Right'?<img style={{float:'right', maxWidth: 200, maxHeight: 200}} src={this.state.imgData}/>:<img style={{float:'left', maxWidth: 200, maxHeight: 200}} src={this.state.imgData}/>}
-              {this.state.updateData===null?<p id="text">{this.state.data[0].quote}</p>:<p id="text">{this.state.updateData.quote}</p>}
-              {this.state.updateData===null?<p id="author">-{this.state.data[0].author}</p>:<p id="author">-{this.state.updateData.author}</p>}
-              <button className="twitter"><a id="tweet-quote" target="_blank" href="https://twitter.com/intent/tweet"><i className="fab fa-twitter"></i></a></button>
-              <button id="new-quote" onClick={this.handleClick}>FeedMeMore</button>
-          </div>
+
+  //   let {input, disabled} = this.state;
+
+  //   if(this.state.input === "00"){
+  //     this.setState({
+  //       input: '0'
+  //     })
+  //   }
+  //   // if(!regex.test(this.state.input)){
+  //   //   this.setState({
+  //   //     disabled: true
+  //   //   })
+  //   // }
+
+  //   else if (input[0] == "." || input[0] == "+" || input[0] == "-" || input[0] == "/" || input[0] == "*") {
+  //     this.setState({
+  //       input: ''
+  //     })
+  // }
+    return (
+      <div className="bg">
+      <div className="wrapper">
+
+        
+
+        <input type="text" id="display" value={this.state.currentNumber}/>
+       
+
+        <div className="gridContainer">
+
+        <button id="clear" value="clear" onClick={this.handleClick} className="operator">AC</button>
+
+        <button id="divide" value="/" onClick={this.handleClick} className="number logic">/</button>
+
+        <button id="multiply" value="*" onClick={this.handleClick} className="number logic">x</button>
+
+        <br/>
+
+        <button id="seven" value="7" onClick={this.handleClick} className="number">7</button>
+
+        <button id="eight" value="8" onClick={this.handleClick} className="number">8</button>
+
+        <button id="nine" value="9" onClick={this.handleClick} className="number">9</button>
+
+        <button id="subtract" value="-" onClick={this.handleClick} className="number logic">-</button>
+
+        <br/>
+
+        <button id="four" value="4" onClick={this.handleClick} className="number">4</button>
+
+        <button id="five" value="5" onClick={this.handleClick} className="number">5</button>
+
+        <button id="six" value="6" onClick={this.handleClick} className="number">6</button>
+
+        <button id="add" value="+" onClick={this.handleClick} className="number logic">+</button>
+
+        <br/>
+
+        <button id="one" value="1" onClick={this.handleClick} className="number">1</button>
+
+        <button id="two" value="2" onClick={this.handleClick} className="number">2</button>
+
+        <button id="three" value="3" onClick={this.handleClick} className="number">3</button>
+
+        <button id="backspace" onClick={this.handleClick} className="number logic">C</button>
+
+        <br/>
+
+        <button id="zero" value="0" onClick={this.handleClick} className="operator">0</button>
+        
+        <button id="decimal" value="." onClick={this.handleClick} className="number">.</button>
+
+        <button id="equals" value="=" onClick={this.handleClick} className="number">=</button>
+
+
+          </div>        
         </div>
-      )
-    }
+      </div>
+    )
   }
 }
 
